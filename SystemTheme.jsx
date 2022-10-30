28b7667f283f45ebf3af7c878d04b34783495190
Ian Main <ian@pandadatasystems.com>
2022-10-30 14:57:47 -0700

    Update SystemTheme.jsx

--- SystemTheme.jsx
+++ SystemTheme.jsx
@@ -13,9 +13,9 @@ function Switch() {
 
     useEffect(() => {
         if (theme === 'dark') {
-            document.documentElement.classList.remove('dark')
-        } else {
             document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
         }
     }, [theme]);
 
@@ -24,9 +24,9 @@ function Switch() {
     };
 
   return (
-    <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in ml-2">
-        <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" onClick={handleThemeSwitch}/>
-        <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+    <div class="relative hidden lg:block w-10 mr-2 align-middle select-none transition duration-200 ease-in ml-2">
+        <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-slate-700 border-4 border-sky-600 appearance-none cursor-pointer" onClick={handleThemeSwitch}/>
+        <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-400 cursor-pointer"></label>
     </div>
 
   )
