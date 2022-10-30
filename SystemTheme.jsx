8a68ecc82bdefdd56139db70ed45a3385902f93a
Ian Main <ian@pandadatasystems.com>
2022-10-30 14:12:00 -0700

    Update SystemTheme.jsx

--- SystemTheme.jsx
+++ SystemTheme.jsx
@@ -24,9 +24,11 @@ function Switch() {
     };
 
   return (
-    <div>
-        <button className='bg-blue-200 p-4 rounded-3xl' onClick={handleThemeSwitch}>Theme</button>
+    <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in ml-2">
+        <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" onClick={handleThemeSwitch}/>
+        <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
     </div>
+
   )
 }
 
